bdc4bb7dbc07f13b1f4cf8b397b22a58bfda5413
Harold Martin <harold.martin@gmail.com>
2017-07-23 10:14:29 -0700

    single adapter setter apparently required for Kotlin support

--- chatkit/src/main/java/com/stfalcon/chatkit/dialogs/DialogsList.java
+++ chatkit/src/main/java/com/stfalcon/chatkit/dialogs/DialogsList.java
@@ -59,23 +59,17 @@ public class DialogsList extends RecyclerView {
     }
 
     /**
-     * Don't use this method for setting your adapter, otherwise exception will by thrown.
-     * Call {@link #setAdapter(DialogsListAdapter)} instead.
+     * Sets adapter for DialogsList
+     *
+     * @param adapter  Adapter MUST extend DialogsListAdapter
      */
     @Override
     public void setAdapter(Adapter adapter) {
-        throw new IllegalArgumentException("You can't set adapter to DialogsList. Use #setAdapter(DialogsListAdapter) instead.");
-    }
-
-    /**
-     * Sets adapter for DialogsList
-     *
-     * @param adapter  Adapter. Must extend DialogsListAdapter
-     * @param <DIALOG> Dialog model class
-     */
-    public <DIALOG extends IDialog>
-    void setAdapter(DialogsListAdapter<DIALOG> adapter) {
-        setAdapter(adapter, true);
+        if (adapter instanceof DialogsListAdapter) {
+            setAdapter((DialogsListAdapter) adapter, true);
+        } else {
+            throw new IllegalArgumentException("Adapter MUST extend DialogsListAdapter");
+        }
     }
 
     /**
